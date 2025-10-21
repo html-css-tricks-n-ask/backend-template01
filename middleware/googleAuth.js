5ed30b2c03ca37256774ab33569a220bedb1c522
html-css-tricks-n-ask <see.bharat20@gmail.com>
2025-10-21 12:18:46 +0530

    unused component

--- middleware/googleAuth.js
+++ middleware/googleAuth.js
@@ -11,7 +11,7 @@ console.log("MONGO_URI:",   process.env.MONGO_URI);
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: process.env.GOOGLE_CALLBACK_URL
+  // callbackURL: process.env.GOOGLE_CALLBACK_URL
 
 },
 async (accessToken, refreshToken, profile, done) => {
